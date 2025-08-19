cfa7a74de4ed86a90f8e3f6a19d8145c1405c84a
Julien Saevecke <julien.saevecke@kbst-gmbh.de>
2025-08-19 10:54:28 +0200

    add migration path env var

--- cmd/gorm-goose-migrations/configuration.go
+++ cmd/gorm-goose-migrations/configuration.go
@@ -5,17 +5,19 @@ import (
 )
 
 type configuration struct {
-	Environment    string
-	LogLevel       string
-	DatabaseURL    string
-	DatabaseDriver string
+	Environment           string
+	LogLevel              string
+	DatabaseURL           string
+	DatabaseDriver        string
+	DatabaseMigrationPath string
 }
 
 func loadConfiguration(getenv func(string) string) configuration {
 	return configuration{
-		Environment:    getenv("ENVIRONMENT"),
-		LogLevel:       getenv("LOG_LEVEL"),
-		DatabaseURL:    getenv("DATABASE_URL"),
-		DatabaseDriver: getenv("DATABASE_DRIVER"),
+		Environment:           getenv("ENVIRONMENT"),
+		LogLevel:              getenv("LOG_LEVEL"),
+		DatabaseURL:           getenv("DATABASE_URL"),
+		DatabaseDriver:        getenv("DATABASE_DRIVER"),
+		DatabaseMigrationPath: getenv("DATABASE_MIGRATION_PATH"),
 	}
 }
